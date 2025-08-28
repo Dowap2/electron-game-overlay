009a472119c8e65228919870374f982d2208f7fd
Dowap2 <dowapdowari@gmail.com>
2025-08-28 16:38:44 +0900

    riot api 해체 분석기

--- dist/tft/fetchTFTData.js
+++ dist/tft/fetchTFTData.js
@@ -9,6 +9,17 @@ const fs_1 = __importDefault(require("fs"));
 const https_1 = __importDefault(require("https"));
 const axios_1 = __importDefault(require("axios"));
 const logger_1 = require("../logger");
+function safeStringify(obj) {
+    const seen = new WeakSet();
+    return JSON.stringify(obj, (key, value) => {
+        if (typeof value === "object" && value !== null) {
+            if (seen.has(value))
+                return "[Circular]";
+            seen.add(value);
+        }
+        return value;
+    }, 2);
+}
 async function fetchTFTData() {
     const possiblePaths = [
         path_1.default.join(process.env.LOCALAPPDATA, "Riot Games/League of Legends/lockfile"),
@@ -25,17 +36,35 @@ async function fetchTFTData() {
     const [, , port, password] = content.split(":");
     const agent = new https_1.default.Agent({ rejectUnauthorized: false });
     try {
-        const phaseRes = await axios_1.default.get(`https://127.0.0.1:${port}/lol-gameflow/v1/gameflow-phase`, { httpsAgent: agent, auth: { username: "riot", password } });
-        (0, logger_1.logToWindow)(`Game phase: ${phaseRes.data}`);
+        // const phaseRes = await axios.get(
+        //   `https://127.0.0.1:${port}/lol-gameflow/v1/gameflow-phase`,
+        //   { httpsAgent: agent, auth: { username: "riot", password } }
+        // );
+        // const phaseRes = await axios.get( //이거 내가 만든 팀 목록 가져오는 api임
+        //   `https://127.0.0.1:${port}/lol-tft-team-planner/v1/sets/dirty`,
+        //   { httpsAgent: agent, auth: { username: "riot", password } }
+        // );
+        const phaseRes = await axios_1.default.get(`https://127.0.0.1:${port}/lol-tft-skill-tree/v1/player-progression`, {
+            httpsAgent: agent,
+            auth: { username: "riot", password },
+        });
+        console.log(safeStringify(phaseRes.data));
+        (0, logger_1.logToWindow)(safeStringify(phaseRes.data));
     }
     catch (err) {
         (0, logger_1.logToWindow)(`Error fetching game phase: ${err.message}`);
     }
     try {
-        const res = await axios_1.default.get("https://127.0.0.1:2999/liveclientdata/allgamedata", {
-            httpsAgent: agent,
-        });
-        (0, logger_1.logToWindow)(`Game mode: ${res.data.gameData.gameMode}`);
+        // const res = await axios.get("https://127.0.0.1:2999/tft-match/v1/players", {
+        //   httpsAgent: agent,
+        // });
+        // const res = await axios.get("https://127.0.0.1:2999/tft-match/v1/game", {
+        //   httpsAgent: agent,
+        // });
+        const res = await axios_1.default.get(`http://127.0.0.1:${port}/swagger/v1/api-docs`, { httpsAgent: agent, auth: { username: "riot", password } });
+        console.log(res);
+        // logToWindow(`res.data:\n${JSON.stringify(res.data, null, 2)}`);
+        (0, logger_1.logToWindow)(`res.data:\n${safeStringify(res.data)}`);
         return [];
     }
     catch (err) {
--- main/tft/fetchTFTData.ts
+++ main/tft/fetchTFTData.ts
@@ -5,6 +5,21 @@ import axios from "axios";
 import { Player } from "../types";
 import { logToWindow } from "../logger";
 
+function safeStringify(obj: any) {
+  const seen = new WeakSet();
+  return JSON.stringify(
+    obj,
+    (key, value) => {
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) return "[Circular]";
+        seen.add(value);
+      }
+      return value;
+    },
+    2
+  );
+}
+
 export async function fetchTFTData(): Promise<Player[]> {
   const possiblePaths = [
     path.join(
@@ -27,23 +42,44 @@ export async function fetchTFTData(): Promise<Player[]> {
   const agent = new https.Agent({ rejectUnauthorized: false });
 
   try {
+    // const phaseRes = await axios.get(
+    //   `https://127.0.0.1:${port}/lol-gameflow/v1/gameflow-phase`,
+    //   { httpsAgent: agent, auth: { username: "riot", password } }
+    // );
+
+    // const phaseRes = await axios.get( //이거 내가 만든 팀 목록 가져오는 api임
+    //   `https://127.0.0.1:${port}/lol-tft-team-planner/v1/sets/dirty`,
+    //   { httpsAgent: agent, auth: { username: "riot", password } }
+    // );
     const phaseRes = await axios.get(
-      `https://127.0.0.1:${port}/lol-gameflow/v1/gameflow-phase`,
-      { httpsAgent: agent, auth: { username: "riot", password } }
+      `https://127.0.0.1:${port}/lol-tft-skill-tree/v1/player-progression`,
+      {
+        httpsAgent: agent,
+        auth: { username: "riot", password },
+      }
     );
-    logToWindow(`Game phase: ${phaseRes.data}`);
+
+    console.log(safeStringify(phaseRes.data));
+    logToWindow(safeStringify(phaseRes.data));
   } catch (err: any) {
     logToWindow(`Error fetching game phase: ${err.message}`);
   }
 
   try {
+    // const res = await axios.get("https://127.0.0.1:2999/tft-match/v1/players", {
+    //   httpsAgent: agent,
+    // });
+
+    // const res = await axios.get("https://127.0.0.1:2999/tft-match/v1/game", {
+    //   httpsAgent: agent,
+    // });
     const res = await axios.get(
-      "https://127.0.0.1:2999/liveclientdata/allgamedata",
-      {
-        httpsAgent: agent,
-      }
+      `http://127.0.0.1:${port}/swagger/v1/api-docs`,
+      { httpsAgent: agent, auth: { username: "riot", password } }
     );
-    logToWindow(`Game mode: ${res.data.gameData.gameMode}`);
+    console.log(res);
+    // logToWindow(`res.data:\n${JSON.stringify(res.data, null, 2)}`);
+    logToWindow(`res.data:\n${safeStringify(res.data)}`);
     return [];
   } catch (err: any) {
     logToWindow(`Error fetching TFT data: ${err.message}`);
